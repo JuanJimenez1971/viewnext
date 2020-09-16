42484db877a9aa3dec0211251b50c7f3a462b368
juan jimenez <sajcjm@hotmail.com>
2020-09-16 17:18:05 +0200

    se añaden docs de javadoc

--- src/main/java/com/example/demo/model/Cliente.java
+++ src/main/java/com/example/demo/model/Cliente.java
@@ -21,12 +21,10 @@ public class Cliente implements Serializable {
 	private String apellidos;
 	private String direccion;
 	private String poblacion;
-	private String provincia;
+	private String provincia;	
 	
+	public Cliente() {	}
 	
-	public Cliente() {
-		super();		
-	}
 	public Cliente(String nombre, String apellidos, String direccion, String poblacion, String provincia) {
 		
 		this.nombre = nombre;
@@ -34,9 +32,7 @@ public class Cliente implements Serializable {
 		this.direccion = direccion;
 		this.poblacion = poblacion;
 		this.provincia = provincia;
-	}
-	
-	
+	}	
 	
 	public Long getId() {
 		return id;
@@ -44,36 +40,43 @@ public class Cliente implements Serializable {
 	public String getNombre() {
 		return nombre;
 	}
+	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
 	public String getApellidos() {
 		return apellidos;
 	}
+	
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
+	
 	public String getDireccion() {
 		return direccion;
 	}
+	
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
+	
 	public String getPoblacion() {
 		return poblacion;
 	}
+	
 	public void setPoblacion(String poblacion) {
 		this.poblacion = poblacion;
 	}
+	
 	public String getProvincia() {
 		return provincia;
 	}
+	
 	public void setProvincia(String provincia) {
 		this.provincia = provincia;
 	}
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
+
 	@Override
 	public String toString() {
 		return "Cliente [id=" + id + ", nombre=" + nombre + ", apellidos=" + apellidos + ", direccion=" + direccion
